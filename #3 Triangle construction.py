64d850e1decbf56fc71d6acbfd5fa32b0451a55e
MatushchakAleksander <112856861+MatushchakAleksander@users.noreply.github.com>
2022-09-19 06:52:09 +0300

    Update #3 Triangle construction.py

--- #3 Triangle construction.py
+++ #3 Triangle construction.py
@@ -1,10 +1,10 @@
 # 3.Вывести треугольник  с шириной N с помощью цикла while
 
-a = int(input('Enter a value for the width of the triangle: '))
-counter = a        
+N = int(input('Enter a value for the width of the triangle: '))
+counter = N        
 counter_1 = -1
 while True:
-    if 0 < counter <= a:
+    if 0 < counter <= N:
         counter = counter-1
         counter_1 = counter_1+1
         print(' '*counter_1+'*'*counter+'*')
